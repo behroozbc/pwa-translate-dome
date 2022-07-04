b5becf94e2907bdc897d840cc81bf9474dee9a31
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2022-07-04 21:45:58 +0430

    add translate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pwa-translate-dome'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pwa-translate-dome');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwa-translate-dome';
+  constructor (public translate: TranslateService) {
+    translate.addLangs(['en', 'fa']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
+    translate.onLangChange.subscribe((langChange: LangChangeEvent) => {
+      const html = document.getElementsByTagName("html")[0];
+      html.setAttribute('lang', langChange.lang);
+      if (langChange.lang == 'fa')
+        html.setAttribute('dir', 'rtl');
+      else
+        html.setAttribute('dir', 'ltr');
+    });
+  }
 }
