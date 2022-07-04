bbce1b2f1253e6187b26b4df446c7a7901eccd73
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2022-07-04 21:59:37 +0430

    fixz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient,'/');
+  return new TranslateHttpLoader(httpClient,'/pwa-translate-dome/');
 }
 @NgModule({
   declarations: [
